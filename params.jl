60d9b0e80b2784bcf78069bd311d10314d575c33
dreiss.isb <dreiss.isb@gmail.com>
2013-08-13 11:02:36 -0700

    updated to actual version

--- params.jl
+++ params.jl
@@ -1,5 +1,3 @@
-## TODO: read in these paramters from cmd-line arguments
-
 if ! isdefined(:organism)
     #organism = "Hpy"
     organism = "Eco"
@@ -7,17 +5,15 @@ if ! isdefined(:organism)
     #organism = "Mpn"
 end
 
-if ! isdefined(:ratios_file) ratios_file = "./junkey/$organism/ratios.tsv"; end
-
 if ! isdefined(:k_clust) 
     if organism == "Hpy" k_clust = 75
-    elseif organism == "Eco" k_clust = 471
+    elseif organism == "Eco" k_clust = 450
     elseif organism == "Sce" k_clust = 500
     elseif organism == "Mpn" k_clust = 75
-        ## DONE: set "default" k_clust if not already set:
-    elseif isdefined(:ratios) k_clust = round( size( ratios, 1 ) / 10 )
     end
 end
+
+if ! isdefined(:ratios_file) ratios_file = "./junkey/$organism/ratios.tsv"; end
 
 if ! isdefined(:n_iters) n_iters = 100; end
 
@@ -33,12 +29,12 @@ if organism == "Sce"
 end
 println( "$distance_search  $distance_scan  $motif_width_range" )
 
-if ! isdefined(:max_network_weight) max_network_weight = 0.03; end 
-if ! isdefined(:max_motif_weight)   max_motif_weight =  0.5; end 
+if ! isdefined(:max_network_weight) max_network_weight = 0.1; end 
+if ! isdefined(:max_motif_weight)   max_motif_weight =   0.8; end 
 
-if ! isdefined(:avg_genes_per_cluster) avg_genes_per_cluster = 31; end 
-if ! isdefined(:avg_clusters_per_gene) avg_clusters_per_gene = 0.7000000000000002; end 
+if ! isdefined(:avg_genes_per_cluster) avg_genes_per_cluster = 22; end 
+if ! isdefined(:avg_clusters_per_gene) avg_clusters_per_gene = 1.3; end 
 
-## allow more updates if there are more clusters??? Tuned to k_clust/2 for Hpy (where k_clust is 75) -- 
+## allow more updates if there are more clusters??? Tuned to k_clust/2 for Hpy (where k_clust is 75) --
 ##     may need additional tuning; e.g. for eco (k_clust=450), k_clust/2 is too high
 if ! isdefined(:max_improvements_per_iter) max_improvements_per_iter = convert(Int64, round(min(k_clust/2, 100))); end
